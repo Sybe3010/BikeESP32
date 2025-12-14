c7862c99848efdb82ba32f2b26a563514b0a68c1
Sybe3010 <sybevandenbergh@outlook.com>
2025-12-14 10:25:00 +0100

    create main files for activitys

---
+++ lib/gui/activityDataPage.hpp
@@ -0,0 +1,11 @@
+#include "globalGuiDef.h"
+
+class ActivityDataPage{
+    private:
+        bool isDataPageLoaded;
+    public:
+        ActivityDataPage();
+
+        void createDataPage();
+        void updateDataPageInformation();
+};
